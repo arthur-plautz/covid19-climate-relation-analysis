435d64dfbbad1cb2ddce73e93e2366396f40f2ac
Arthur Plautz Ventura <atr.plautz@gmail.com>
2020-10-11 02:13:13 -0300

    First Data Processing & Load

--- models/data_extraction.py
+++ models/data_extraction.py
@@ -1,7 +1,38 @@
 import requests
+import os
+import pandas as pd
+from horology import timed
 
-state = 'sc'
-url = f'https://s3-sa-east-1.amazonaws.com/ckan.saude.gov.br/dados-{state}.csv'
+CURRENT_DIR = os.getcwd()
 
-r = requests.get(url, allow_redirects=True)
-open(f'../data/covid_{state}_notifications.csv', 'wb').write(r.content)
+@timed
+def download_data(local_url, source_url):
+    r = requests.get(source_url, allow_redirects=True)
+    open(local_url, 'wb').write(r.content)
+    print('File Size: '+os.stat(local_url).st_size) 
+
+def extract_covid_data(source_url, uf):
+    local_url = f"{CURRENT_DIR}/data/covid_{uf['initials'].lower()}.csv"
+    download_data(local_url, source_url)
+    return local_url
+
+def load_covid_data(uf):
+    data_file = f"{CURRENT_DIR}/data/covid_{uf['initials'].lower()}.csv"
+    df = pd.read_csv(data_file, encoding='ISO-8859-1', sep=';', error_bad_lines=False,index_col=False)
+    df['id'] = range(1, len(df) + 1)
+    return df
+
+def load_counties_data(uf):
+    data_file = f"{CURRENT_DIR}/data/county_list.csv"
+    df = pd.read_csv(data_file)
+    return df.query(f"COD_UF == {uf['code']}")
+
+def load_uf_data(initials):
+    data_file = f"{CURRENT_DIR}/data/uf_list.csv"
+    df = pd.read_csv(data_file)
+    uf = df.query(f"SIGLA == '{initials}'")
+    return {
+        'nome': uf['NOME'].item(),
+        'code': uf['COD'].item(),
+        'initials': uf['SIGLA'].item()
+    }
