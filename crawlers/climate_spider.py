881c0d5132dec92e0081dc79a18e59ebcf9674ed
Arthur Plautz Ventura <atr.plautz@gmail.com>
2020-10-12 19:14:11 -0300

    Fixing Climate Metrics

--- crawlers/climate_spider.py
+++ crawlers/climate_spider.py
@@ -9,10 +9,10 @@ class ClimateSpider(scrapy.Spider):
         for hour in response.css('table#wt-his>tbody>tr'):
             hour_metrics = {}
             metric_counter = 0
-            metrics = [None, None, 'AT', None, 'W', 'RH']
+            metrics = ['AT', None, 'W', None, 'RH']
             hour_metrics['time'] = hour.css('th ::text').get()
             for metric in hour.css('td ::text'):
-                if metric_counter in [2,4,5]:
+                if metric_counter in [0,2,4]:
                     hour_metrics[metrics[metric_counter]] = metric.get()
                 metric_counter +=1
             yield hour_metrics
