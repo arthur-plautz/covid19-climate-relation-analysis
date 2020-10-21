e2669af239a03b8835876ab123c28384fdadeabb
Gabriel Garcia Deitos <gabrielgarciadeitos@hotmail.com>
2020-10-20 21:35:27 -0300

    created method select_infection_period and climate_data_dict

--- master.py
+++ master.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
-from models.data_processing import select_counties
-from models.data_extraction import load_covid_data, extract_covid_data, load_uf_data, load_counties_data, extract_climate_data
+from models.data_processing import select_counties, select_infection_period
+from models.data_extraction import load_covid_data, extract_covid_data, load_uf_data, load_counties_data, extract_climate_data, load_climate_data
 
 uf = load_uf_data(
     os.environ.get('uf', 'SC')
@@ -9,4 +9,8 @@ uf = load_uf_data(
 covid_uf_cases = load_covid_data(uf)
 cases_sample = select_counties(covid_uf_cases, uf)
 counties = cases_sample.municipio_notificacao.unique()
-extract_climate_data(counties, uf['nome'])
+#extract_climate_data(counties, uf['nome'])
+print(counties)
+df = load_climate_data('FLORIANOPOLIS')
+print(df.info())
+print(select_infection_period(df, '2020-04-01'))
--- models/data_processing.py
+++ models/data_processing.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 import math
-from models.data_extraction import load_counties_data
+from calendar import monthrange
+from models.data_extraction import load_counties_data, load_climate_data
 
 def iterate_query_values(df, column):
     selection = ''
@@ -29,3 +30,39 @@ def select_counties(covid_df, uf):
     print(f"Counties: {len(cases_county)}")
     selected_counties = iterate_query_values(cases_county, 'municipio_notificacao')
     return covid_df.query(f"municipio_notificacao in ({selected_counties})")
+
+def select_infection_period(climate_df, start_date):
+    year, month, day = start_date.split('-')
+    day_int = int(day)
+    month_int = int(month)
+    month_dict = {1: 31, 2: 29, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+    inicial_date = day_int - 10
+    list_date = []
+    if inicial_date < 1:
+        previous_month_value = month_dict[month_int - 1]
+        previous_month = month_int - 1
+        for i in range(abs(inicial_date)+1):
+            if i < 10:
+                list_date.append(year + '-0' + str(previous_month) + '-' + str(previous_month_value + inicial_date + i))
+            else:
+                list_date.append(year + '-' + str(previous_month) + '-' + str(previous_month_value + inicial_date + i))
+        for i in range(1, day_int):
+            if i < 10:
+                list_date.append(year + '-' + month + '-0' + str(i))
+            else:
+                list_date.append(year + '-' + month + '-' + str(i))
+    else:
+        for i in range(day_int - 10, day_int):
+            if i < 10:
+                list_date.append(year + '-' + month + '-0' + str(i))
+            else:
+                list_date.append(year + '-' + month + '-' + str(i))
+    infection_period_df = climate_df.loc[climate_df['date'].isin(list_date)]
+    return infection_period_df
+
+def climate_data_dict(df, counties_unique_values):
+    climate_dict = {}
+    for county in counties_unique_values:
+        df_new = load_climate_data(county)
+        climate_dict[county.replace(" ", "_")] = df_new
+    return climate_dict
