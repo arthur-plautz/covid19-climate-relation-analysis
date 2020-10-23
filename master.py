af9e6dbf63a9c0a710bc30abee3f906f7b78d92f
Arthur Plautz Ventura <atr.plautz@gmail.com>
2020-10-22 21:48:41 -0300

    variation and rate functions for time series

--- master.py
+++ master.py
@@ -1,38 +1,14 @@
 import pandas as pd
 import os
 import numpy as np
-from models.data_processing import remove_values
+from models.data_processing import remove_values, time_series, series_rate, series_variation
 from models.data_load import read_processed, basic_processing
-import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 covid_cases = read_processed('covid_cases')
 cases_climate = read_processed('cases_climate')
 
 cases_climate.date = pd.to_datetime(cases_climate.date)
 cases_climate = cases_climate.query("municipio == 'FLORIANOPOLIS'").sort_values('date')
-covid_cases.data_inicio_sintomas = pd.to_datetime(covid_cases.data_inicio_sintomas)
-cases_count = covid_cases.groupby('data_inicio_sintomas').count()
 
-cases_model = cases_count.resample('1D').sum()
-
-trace1 = go.Scatter(
-    x=cases_model.index,
-    y=cases_model.id,
-    yaxis="y1"
-)
-trace2 = go.Scatter(
-    x=cases_climate.date,
-    y=cases_climate.AT,
-    yaxis="y2"
-)
-data = [trace1, trace2]
-layout = go.Layout(
-    yaxis=dict(
-        domain=[1/4, 1]
-    ),
-    yaxis2=dict(
-        domain=[0, 1/4]
-    )
-)
-fig = go.Figure(data=data, layout=layout)
-fig.show()
+covid_cases_serie = time_series(covid_cases, 'data_inicio_sintomas')
